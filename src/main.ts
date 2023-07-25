c53e54e0bfbf948cc74c09b4bfe5ba8ec2c32e46
jakeszhang <jakeszhang@qt-asia.com>
2023-07-25 16:33:19 +0800

    feat(other): 优化

--- src/main.ts
+++ src/main.ts
@@ -14,17 +14,21 @@ import '@/styles/index.scss';
 async function bootstrap(): Promise<void> {
   const app = createApp(App);
   app.use(router).use(store).use(i18n).use(Menu).use(Tooltip);
-  await router.isReady();
-  app.mount('#app', true);
-}
 
-async function useVxeTable(): Promise<void> {
   if (import.meta.env.PROD) {
     await setToken(window.localStorage.getItem('SSID')!);
     await setLang(window.localStorage.getItem('Language')!);
   }
   await getPublicApi();
 
+  await router.isReady();
+  app.mount('#app', true);
+  app.config.errorHandler = (err: Error) => {
+    console.log(err);
+  };
+}
+
+async function useVxeTable(): Promise<void> {
   VXETable.setup({
     //@ts-ignore
     i18n: (key, args) => i18n.global.t(key, args),
